71fea857cc34b55c5c4ed8a729dc16c965e96904
Julian Thatcher <daedalus.raistlin@gmail.com>
2017-05-07 22:14:07 +1000

    Add timing

--- PyLithp/PyLithp.py
+++ PyLithp/PyLithp.py
@@ -6,11 +6,15 @@
 from lithptypes  import *
 from builtins    import Builtins
 from interpreter import Interpreter
+import time
 
 if __name__ == "__main__":
+	t0 = time.time()
 	builtins = Builtins()
 	chain = OpChain()
 	builtins.fillClosure(chain.closure)
+	t1 = time.time()
+	print "Standard library loaded in ", t1 - t0
 
 	fndef = FunctionDefinition(["A", "B"], OpChain(None, [
 		FunctionCall("+/*", [
@@ -23,9 +27,14 @@ if __name__ == "__main__":
 	])
 	chain.add(fncall)
 	fncall = FunctionCall("print/2",
-		[Literal("Testing: "),
+		[Literal("Testing:"),
 		 FunctionCall("add/2", [Literal(2), Literal(3)])])
 	chain.add(fncall)
-
 	interp = Interpreter()
+
+	t2 = time.time()
+	print "Code compiled in ", t2 - t1
+
 	interp.run(chain)
+	t3 = time.time()
+	print "Execution done in ", t3 - t2
